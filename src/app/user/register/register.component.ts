6ffffa57980eca88a3039fda0b3710c8b2d91bf5
Sachin Chanduveettil <sachisavera@gmail.com>
2023-03-10 21:50:06 +0530

    angular reactive register forms ui

---
+++ src/app/user/register/register.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup} from '@angular/forms'
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit{
+    constructor(){}
+
+    ngOnInit(): void {
+      
+    }
+    registerForm = new FormGroup({
+      firstname:  new FormControl(""),
+      lastname:  new FormControl(""),
+      email:  new FormControl(""),
+      mobile:  new FormControl(""),
+      gender:  new FormControl(""),
+      pwd:  new FormControl(""),
+      cpwd:  new FormControl("")
+    });
+    registerPass(){
+      console.log(this.registerForm.value);
+    }
+}
